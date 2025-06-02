df34d7234e32f90b27ddf00759a01ff2ef0c0d30
Sergey Okhotnikov <ognerezov@gmail.com>
2025-06-02 08:12:32 +0000

    completed workshop

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,81 @@
+import { useCallback, useMemo, useState } from 'react'
 import products from './products.json'
 import { Product } from 'types'
 
 const App = () => {
   const isError = false
+  const [currentProduct, setCurrentProduct] = useState(null)
+  const currentPrice = useMemo(
+    () => currentProduct?.price || 0,
+    [currentProduct],
+  )
+  const [weight, setWeight] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [cart, setCart] = useState([])
+
+  const cartTotal = useMemo(() => {
+    return cart.reduce((acc, prod) => acc + prod.total, 0)
+  }, [cart])
+
+  const calculate = useCallback(() => {
+    if (!weight) {
+      return
+    }
+    setTotal(weight * currentPrice)
+    console.log(weight)
+    console.log(currentPrice)
+    setCart((currentCart) => {
+      return [
+        ...currentCart,
+        { ...currentProduct, total: weight * currentPrice },
+      ]
+    })
+  }, [weight, currentPrice, currentProduct])
+
+  const updateWeight = useCallback((e) => {
+    console.log(e?.target.value)
+    console.log(parseFloat(e?.target.value))
+    setWeight(parseFloat(e?.target.value))
+  }, [])
+
+  const setProduct = useCallback((product: any) => {
+    setCurrentProduct(product)
+    setWeight(0)
+  }, [])
 
   return (
     <div className="app">
       <div className="display">
         <label htmlFor="weight">
           Peso:
-          <input id="weight" name="weight" type="number" placeholder="0,000" />
+          <input
+            id="weight"
+            name="weight"
+            type="number"
+            placeholder="0,000"
+            onChange={updateWeight}
+            value={weight}
+          />
         </label>
 
         <label htmlFor="price">
           Precio:
-          <input id="price" type="number" placeholder="0,000" readOnly />
+          <input
+            id="price"
+            type="number"
+            placeholder="0,000"
+            value={currentPrice}
+          />
         </label>
         <label htmlFor="total">
           Total:
-          <input id="total" type="number" placeholder="0,000" readOnly />
+          <input
+            id="total"
+            type="number"
+            placeholder="0,000"
+            readOnly
+            value={total === 0 ? undefined : total}
+          />
         </label>
       </div>
       <div className="controls">
@@ -29,7 +86,7 @@ const App = () => {
                 key={product.id}
                 aria-label={product.name}
                 value={product.price}
-                onClick={() => null}
+                onClick={() => setProduct(product)}
               >
                 <img src={product.image} alt="" />
               </button>
@@ -38,8 +95,15 @@ const App = () => {
         </div>
         <aside className="sidebar">
           <div>
-            <button>Calcular</button>
+            <button onClick={calculate}>Calcular</button>
           </div>
+
+          {cart.map((product) => (
+            <div
+              style={{ fontWeight: 300 }}
+            >{`${product.name} - ${product.total.toFixed(2)}€`}</div>
+          ))}
+          <div>{`Total - ${cartTotal.toFixed(2)}€`}</div>
         </aside>
       </div>
       {isError && <div className="error"></div>}
--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'
-
+import products from '../products.json'
+import userEvent from '@testing-library/user-event'
 import App from '../App'
 
 it('should see the scale app', () => {
@@ -19,3 +20,133 @@ it('should see the scale app', () => {
   expect(mango).toBeInTheDocument()
   expect(calculateButton).toBeInTheDocument()
 })
+
+it('should see Precio', async () => {
+  render(<App />)
+
+  const priceInput = screen.getByLabelText('Precio:')
+
+  const orange = screen.getByLabelText('Naranja')
+
+  await userEvent.click(orange)
+  expect(priceInput).toHaveValue(1.55)
+})
+
+it('should calculate Total', async () => {
+  render(<App />)
+
+  const weightInput = screen.getByLabelText('Peso:')
+  const orange = screen.getByLabelText('Naranja')
+  const totalInput = screen.getByLabelText('Total:')
+  const calculateButton = screen.getByRole('button', { name: 'Calcular' })
+
+  await userEvent.click(orange)
+  await userEvent.type(weightInput, '100')
+  await userEvent.click(calculateButton)
+
+  expect(totalInput).toHaveValue(155)
+})
+
+it('Set price to 0', async () => {
+  render(<App />)
+
+  const weightInput = screen.getByLabelText('Peso:')
+  const orange = screen.getByLabelText('Naranja')
+  const mango = screen.getByLabelText('Mango')
+
+  await userEvent.click(orange)
+  await userEvent.type(weightInput, '100')
+
+  await userEvent.click(mango)
+
+  expect(weightInput).toHaveValue(0)
+})
+
+it('should calculate Total and display for 1 product', async () => {
+  render(<App />)
+
+  const weightInput = screen.getByLabelText('Peso:')
+  const orange = screen.getByLabelText('Naranja')
+  const totalInput = screen.getByLabelText('Total:')
+  const calculateButton = screen.getByRole('button', { name: 'Calcular' })
+
+  await userEvent.click(orange)
+  await userEvent.type(weightInput, '100')
+  await userEvent.click(calculateButton)
+
+  expect(totalInput).toHaveValue(155)
+  const orangeText = screen.getByText(`Naranja - ${'155.00'}€`)
+  const totalText = screen.getByText(`Total - ${'155.00'}€`)
+
+  expect(orangeText).toBeInTheDocument()
+  expect(totalText).toBeInTheDocument()
+})
+
+it('should calculate Total and display for multiple products', async () => {
+  render(<App />)
+
+  const weightInput = screen.getByLabelText('Peso:')
+  const orange = screen.getByLabelText('Naranja')
+  const totalInput = screen.getByLabelText('Total:')
+  const calculateButton = screen.getByRole('button', { name: 'Calcular' })
+  const banana = screen.getByLabelText('Plátano')
+  const mango = screen.getByLabelText('Mango')
+
+  await userEvent.click(orange)
+  await userEvent.type(weightInput, '100')
+  await userEvent.click(calculateButton)
+
+  await userEvent.click(banana)
+  await userEvent.type(weightInput, '10')
+  await userEvent.click(calculateButton)
+
+  await userEvent.click(mango)
+  await userEvent.type(weightInput, '1')
+  await userEvent.click(calculateButton)
+
+  const bananaText = screen.getByText(`Plátano - ${'16.90'}€`)
+  const mangoText = screen.getByText(`Mango - ${2.35}€`)
+  const orangeText = screen.getByText(`Naranja - ${'155.00'}€`)
+  const totalText = screen.getByText(`Total - ${174.25}€`)
+
+  expect(bananaText).toBeInTheDocument()
+  expect(mangoText).toBeInTheDocument()
+  expect(orangeText).toBeInTheDocument()
+  expect(totalText).toBeInTheDocument()
+})
+
+it('should not calculate Total if weight = 0', async () => {
+  render(<App />)
+
+  const orange = screen.getByLabelText('Naranja')
+
+  const calculateButton = screen.getByRole('button', { name: 'Calcular' })
+
+  await userEvent.click(orange)
+  await userEvent.click(calculateButton)
+
+  const totalText = screen.queryByText('Total - NaN€')
+  expect(totalText).not.toBeInTheDocument()
+})
+
+// it('should add the same product with different weights', async () => {
+//   render(<App />)
+
+//   const orange = screen.getByLabelText('Naranja')
+
+//   const calculateButton = screen.getByRole('button', { name: 'Calcular' })
+
+//   await userEvent.click(orange)
+//   await userEvent.type(weightInput, '1')
+//   await userEvent.click(calculateButton)
+
+//   await userEvent.clear(weightInput)
+//   await userEvent.type(weightInput, '10')
+//   await userEvent.click(calculateButton)
+
+//   const orangeText1 = screen.getByText(`Naranja - ${'155.00'}€`)
+//   const totalText = screen.getByText(`Total - ${174.25}€`)
+
+//   expect(orangeText).toBeInTheDocument()
+//   expect(totalText).toBeInTheDocument()
+// })
